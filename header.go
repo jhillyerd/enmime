584f57d7439b9d630ec9e701c8e3c9ed59bd348a
James Hillyerd <james@hillyerd.com>
2013-09-29 16:48:30 -0700

    Refactored parser
    Watched Rob Pike's talk on the go template lexer, and decided to copy
    his state machine design.  Should make the code easier to understand.

--- header.go
+++ header.go
@@ -8,142 +8,254 @@ import (
 	"strings"
 )
 
+func debug(format string, args ...interface{}) {
+	if false {
+		fmt.Printf(format, args...)
+		fmt.Println()
+	}
+}
+
 // Terminology from RFC 2047:
 //  encoded-word: the entire =?charset?encoding?encoded-text?= string
 //  charset: the character set portion of the encoded word
 //  encoding: the character encoding type used for the encoded-text
 //  encoded-text: the text we are decoding
-const (
-	PLAIN    int = iota // In plain text
-	RESET               // Parse error, output encoded-word as plaintext
-	CHARSET             // In charset name
-	ENCODING            // In encoding name
-	ENCTEXT             // In encoded-text
-	SPACE               // Space following an encoded-word
-)
+
+// State function modeled on Rob Pike's lexer talk (see source of
+// Go's text/template/parser/lex.go)
+type stateFn func(*headerDec) stateFn
+
+const eof = -1
+
+// headerDec holds the state of the scanner and an output buffer
+type headerDec struct {
+	input    []byte  // Input to decode
+	state    stateFn // Next state
+	start    int     // Start of text we don't yet know what to do with
+	pos      int     // Current parsing position
+	charset  string  // Character set of current encoded word
+	encoding string  // Encoding of current encoded word
+	outbuf   bytes.Buffer
+}
+
+// next returns the next rune in the input
+func (h *headerDec) next() rune {
+	if h.pos >= len(h.input) {
+		return eof
+	}
+	r := h.input[h.pos]
+	h.pos++
+	return rune(r)
+}
+
+// backup a single rune
+func (h *headerDec) backup() {
+	if h.pos > 0 {
+		h.pos--
+	}
+}
+
+// peek at the next rune without consuming it
+func (h *headerDec) peek() rune {
+	r := h.next()
+	h.backup()
+	return r
+}
+
+// ignore will forget all input between start and pos
+func (h *headerDec) ignore() {
+	h.start = h.pos
+}
+
+// output will append all input from start to pos (inclusive) to outbuf
+func (h *headerDec) output() {
+	if h.pos > h.start {
+		h.outbuf.Write(h.input[h.start : h.pos])
+		h.start = h.pos
+	}
+}
+
+// accept consumes the next rune if it's part of the valid set
+func (h *headerDec) accept(valid string) bool {
+	if r := h.next(); r != eof {
+		if strings.IndexRune(valid, r) >= 0 {
+			return true
+		}
+		h.backup()
+	}
+	return false
+}
 
 // Decode a MIME header per RFC 2047
 func decodeHeader(input string) (utf8 string, err error) {
-	if ! strings.Contains(input, "=?") {
-		// Don't scan if there is nothing to do here
-		return input, nil
+	//if ! strings.Contains(input, "=?") {
+	//	// Don't scan if there is nothing to do here
+	//	return input, nil
+	//}
+
+	h := &headerDec{
+		input: []byte(input),
+		state: plainState,
 	}
 
-	outbuf := new(bytes.Buffer)
-	state := PLAIN
-	startPos := 0
-	var charsetBytes, encodingBytes, encTextBytes []byte
+	debug("Starting parse of: '%v'\n", input)
 
-	for pos := 0; pos < len(input); pos++ {
-		ch := input[pos]
-		switch state {
-		case SPACE:
-			// Eat space characters only between encoded words
-			switch ch {
-			case ' ', '\t', '\r', '\n':
-				// Still in space
-				continue
-			case '=':
-				if len(input) > pos+1 && input[pos+1] == '?' {
-					// Start of new encoded word.  If the word is valid, we want to eat
-					// the whitespace.  If not, startPos was defined in transition to SPACE,
-					// and we will output it.
-					pos++
-					state = CHARSET
-					charsetBytes = make([]byte, 0, 128)
-					continue
-				}
-			}
-			// We hit plain text, will need to output whitespace
-			for i := startPos; i <= pos; i++ {
-				outbuf.WriteByte(input[i])
-			}
-			state = PLAIN
-		case PLAIN:
-			// Scan for start of encoded word: =?
-			if ch == '=' && len(input) > pos+1 && input[pos+1] == '?' {
-				// Save start in case this turns out to be corrupt
-				startPos = pos
-				pos++
-				state = CHARSET
-				charsetBytes = make([]byte, 0, 128)
-				continue
-			}
+	for h.state != nil {
+		h.state = h.state(h)
+	}
 
-			// Plain text
-			outbuf.WriteByte(ch)
-		case RESET:
-			// There was a problem parsing an encoded-word, recover by outputting
-			// plain-text until next encoded word
-			for i := startPos; i <= pos; i++ {
-				outbuf.WriteByte(input[i])
-			}
-			state = PLAIN
-		case CHARSET:
-			// Parse character set
-			switch {
-			case isTokenChar(ch):
-				// Part of charset name
-				charsetBytes = append(charsetBytes, ch)
-			case ch == '?':
-				// End of charset name
-				state = ENCODING
-				encodingBytes = make([]byte, 0, 128)
-			default:
-				state = RESET
-			}
-		case ENCODING:
-			// Parse encoding
-			switch {
-			case isTokenChar(ch):
-				// Part of encoding name
-				encodingBytes = append(encodingBytes, ch)
-			case ch == '?':
-				// End of encoding
-				state = ENCTEXT
-				encTextBytes = make([]byte, 0, 128)
-			default:
-				state = RESET
-			}
-		case ENCTEXT:
-			// Decode encoded-text
-			switch {
-			case ch < 33:
-				// No controls or space allowed
-				state = RESET
-			case ch > 126:
-				// No DEL or extended ascii allowed
-				state = RESET
-			case ch == '?':
-				if len(input) > pos+1 && input[pos+1] == '=' {
-					text, err := convertText(charsetBytes, encodingBytes, encTextBytes)
-					if err == nil {
-						outbuf.WriteString(text)
-						pos++
-						// Entering post-word space
-						state = SPACE
-						startPos = pos+1
-					} else {
-						// Conversion failed
-						state = RESET
-					}
-				} else {
-					// Invalid termination
-					state = RESET
-				}
-			default:
-				encTextBytes = append(encTextBytes, ch)
+	return h.outbuf.String(), nil
+}
+
+// State: Reset, output mangled encoded-word as plaintext
+//
+// There was a problem parsing an encoded-word, recover by outputting
+// plain-text until next encoded word
+func resetState(h *headerDec) stateFn {
+	debug("entering reset state with buf %q", h.outbuf.String())
+	h.output()
+	return plainState
+}
+
+// State: In plain text
+func plainState(h *headerDec) stateFn {
+	debug("entering plain state with buf %q", h.outbuf.String())
+	for r := h.next(); r != eof; r = h.next() {
+		//fmt.Printf(" %q\n", r)
+		// Scan for start of encoded word: =?
+		if r == '=' {
+			// Dump out preceeding plaintext, w/o =
+			h.backup()
+			h.output()
+			h.next()
+			if h.accept("?") {
+				return charsetState
 			}
 		}
 	}
+	// Hitting EOF in plain state means we are done
+	h.output()
+	return nil
+}
 
-	return outbuf.String(), nil
+// State: In charset name
+func charsetState(h *headerDec) stateFn {
+	debug("entering charset state with buf %q", h.outbuf.String())
+	myStart := h.pos
+	for r := h.next(); r != eof; r = h.next() {
+		// Parse character set
+		switch {
+		case isTokenChar(r):
+			// Part of charset name, keep going
+		case r == '?':
+			// End of charset name
+			h.charset = string(h.input[myStart : h.pos-1])
+			debug("charset %q", h.charset)
+			return encodingState
+		default:
+			// Invalid character
+			return resetState
+		}
+	}
+	// Hit eof!
+	return resetState
+}
+
+// State: In encoding name
+func encodingState(h *headerDec) stateFn {
+	debug("entering encoding state with buf %q", h.outbuf.String())
+	myStart := h.pos
+	for r := h.next(); r != eof; r = h.next() {
+		// Parse encoding
+		switch {
+		case isTokenChar(r):
+			// Part of encoding name, keep going
+		case r == '?':
+			// End of encoding name
+			h.encoding = string(h.input[myStart : h.pos-1])
+			debug("encoding %q", h.encoding)
+			return encTextState
+		default:
+			// Invalid character
+			return resetState
+		}
+	}
+	// Hit eof!
+	return resetState
+}
+
+// State: In encoded-text
+func encTextState(h *headerDec) stateFn {
+	debug("entering encText state with buf %q", h.outbuf.String())
+	myStart := h.pos
+	for r := h.next(); r != eof; r = h.next() {
+		// Decode encoded-text
+		switch {
+		case r < 33:
+			// No controls or space allowed
+			debug("Encountered control character")
+			return resetState
+		case r > 126:
+			// No DEL or extended ascii allowed
+			debug("Encountered DEL or extended ascii")
+			return resetState
+		case r == '?':
+			if h.accept("=") {
+				text, err := convertText(h.charset, h.encoding, h.input[myStart:h.pos-2])
+				if err == nil {
+					debug("Text converted to: %q", text)
+					h.outbuf.WriteString(text)
+					h.ignore()
+					// Entering post-word space
+					return spaceState
+				} else {
+					// Conversion failed
+					debug("Text conversion failed: %q", err)
+					return resetState
+				}
+			} else {
+				// Invalid termination
+				debug("Invalid termination")
+				return resetState
+			}
+		}
+	}
+	// Hit eof!
+	return resetState
+}
+
+// State: White space following an encoded-word
+func spaceState(h *headerDec) stateFn {
+	debug("entering space state with buf %q", h.outbuf.String())
+Loop:
+	for {
+		// Eat space characters only between encoded words
+		switch {
+		case h.accept(" \t\r\n"):
+			debug("In space")
+			// Still in space
+		case h.accept("="):
+			debug("In =")
+			if h.accept("?") {
+				// Start of new encoded word.  If the word is valid, we want to eat
+				// the whitespace.  If not, h.start was set in transition to SPACE,
+				// and we will output the space.
+				return charsetState
+			}
+		default:
+			debug("In default")
+			break Loop
+		}
+	}
+	debug("In plain")
+	// We hit plain text, will need to output whitespace
+	h.output()
+	return plainState
 }
 
 // Convert the encTextBytes to UTF-8 and return as a string
-func convertText(charsetBytes, encodingBytes, encTextBytes []byte) (string, error) {
-	encoding := strings.ToLower(string(encodingBytes))
-	switch encoding {
+func convertText(charset string, encoding string, encTextBytes []byte) (string, error) {
+	switch strings.ToLower(encoding) {
 	case "b":
 		// Base64 encoded
 		textBytes, err := decodeBase64(encTextBytes)
@@ -154,7 +266,7 @@ func convertText(charsetBytes, encodingBytes, encTextBytes []byte) (string, erro
 		return string(textBytes), err
 	default:
 		return "", fmt.Errorf("Invalid encoding: %v", encoding)
-	}	
+	}
 }
 
 func decodeQuotedPrintable(input []byte) ([]byte, error) {
@@ -187,7 +299,7 @@ func decodeBase64(input []byte) ([]byte, error) {
 }
 
 // Is this an especial character per RFC 2047
-func isEspecialChar(ch byte) bool {
+func isEspecialChar(ch rune) bool {
 	switch ch {
 	case '(', ')', '<', '>', '@', ',', ';', ':':
 		return true
@@ -198,7 +310,7 @@ func isEspecialChar(ch byte) bool {
 }
 
 // Is this a "token" character per RFC 2047
-func isTokenChar(ch byte) bool {
+func isTokenChar(ch rune) bool {
 	// No controls or space
 	if ch < 33 {
 		return false
