406703fca46968f17b7463a71567bb0e1b08cff0
James Hillyerd <james@hillyerd.com>
2016-11-16 21:04:55 -0800

    Package doc introduction rewritten

--- doc.go
+++ doc.go
@@ -1,23 +1,37 @@
-// Package enmime implements a MIME parsing library for Go.  It's built ontop of Go's included
+// Package enmime implements a MIME parsing library for Go.  It's built on top of Go's included
 // mime/multipart support, but is geared towards parsing MIME encoded emails.
 //
-// The Basics
+// Overview
 //
-// Calling EnvelopeFromMessage causes enmime to parse the body of the message object into a tree of
-// Part objects, each of which is aware of its content type, filename and headers.  If the part was
-// encoded in quoted-printable or base64, it is decoded before being stored in the Part object.
+// The enmime API has two conceptual layers.  The lower layer is a tree of Part structs,
+// representing each component of a decoded MIME message.  The upper layer, called an Envelope
+// provides an intuitive way to interact with a MIME message.
 //
-// EnvelopeFromMessage returns an Envelope struct.  The struct contains both the plain text and HTML
-// portions of the email (if available).  The root of the tree, as well as slices of the email's
-// inlines and attachments are also available.
+// Part Tree
 //
-// If you need to locate a particular Part, you can pass a custom PartMatcher function into
-// BreadthMatchFirst() or DepthMatchFirst() to search the Part tree.  BreadthMatchAll() and
-// DepthMatchAll() will collect all matching parts.
+// Calling ReadParts causes enmime to parse the body of a MIME message into a tree of Part objects,
+// each of which is aware of its content type, filename and headers.  Each Part implements
+// io.Reader, providing access to the content it represents.  If the part was encoded in
+// quoted-printable or base64, it is decoded prior to being accessed by the Reader.
+//
+// If you need to locate a particular Part, you can pass a custom PartMatcher function into the
+// BreadthMatchFirst() or DepthMatchFirst() methods to search the Part tree.  BreadthMatchAll() and
+// DepthMatchAll() will collect all Parts matching your criteria.
+//
+// The Envelope
+//
+// EnvelopeFromMessage returns an Envelope struct.  Behind the scenes a Part tree is constructed,
+// and then sorted into the correct fields of the Envelope.
+//
+// The Envelope contains both the plain text and HTML portions of the email.  If there was no plain
+// text Part available, the HTML Part will be downconverted using the html2text library[1].  The
+// root of the Part tree, as well as slices of the inline and attachment Parts are also available.
 //
 // Please note that enmime parses messages into memory, so it is not likely to perform well with
 // multi-gigabyte attachments.
 //
 // enmime is open source software released under the MIT License.  The latest version can be found
 // at https://github.com/jhillyerd/go.enmime
+//
+// [1]: https://github.com/jaytaylor/html2text
 package enmime
