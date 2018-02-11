2d596a6aa58384ff396e4fed1f249e3903327fe3
James Hillyerd <james@hillyerd.com>
2018-02-11 14:53:27 -0800

    Reduce complexity of Encode()
    We don't need to check all Write() errors because bufio.Writer will
    return them when flushed.

--- encode.go
+++ encode.go
@@ -30,17 +30,51 @@ var crnl = []byte{'\r', '\n'}
 
 // Encode writes this Part and all its children to the specified writer in MIME format.
 func (p *Part) Encode(writer io.Writer) error {
-	// Determine content transfer encoding.
-	cte := te7Bit
 	if p.Header == nil {
 		p.Header = make(textproto.MIMEHeader)
 	}
+	cte := p.setupMIMEHeaders()
+	// Encode this part.
+	b := bufio.NewWriter(writer)
+	p.encodeHeader(b)
+	if len(p.Content) > 0 {
+		b.Write(crnl)
+		if err := p.encodeContent(b, cte); err != nil {
+			return err
+		}
+		b.Write(crnl)
+	}
+	if p.FirstChild == nil {
+		return b.Flush()
+	}
+	// Encode children.
+	endMarker := []byte("\r\n--" + p.Boundary + "--")
+	marker := endMarker[:len(endMarker)-2]
+	c := p.FirstChild
+	for c != nil {
+		b.Write(marker)
+		b.Write(crnl)
+		if err := c.Encode(b); err != nil {
+			return err
+		}
+		c = c.NextSibling
+	}
+	b.Write(endMarker)
+	b.Write(crnl)
+	return b.Flush()
+}
+
+// setupMIMEHeaders determines content transfer encoding, generates a boundary string if required,
+// then sets the Content-Type (type, charset, filename, boundary) and Content-Disposition headers.
+func (p *Part) setupMIMEHeaders() transferEncoding {
+	// Determine content transfer encoding.
+	cte := te7Bit
 	if len(p.Content) > 0 {
 		cte = teBase64
 		if p.TextContent() {
 			cte = selectTransferEncoding(p.Content, false)
 			if p.Charset == "" {
-				p.Charset = utf8 // Default
+				p.Charset = utf8
 			}
 		}
 		// RFC 2045: 7bit is assumed if CTE header not present.
@@ -54,8 +88,7 @@ func (p *Part) Encode(writer io.Writer) error {
 	// Setup headers.
 	if p.FirstChild != nil && p.Boundary == "" {
 		// Multipart, generate random boundary marker.
-		uuid := stringutil.UUID()
-		p.Boundary = "enmime-" + uuid
+		p.Boundary = "enmime-" + stringutil.UUID()
 	}
 	if p.ContentID != "" {
 		p.Header.Set(hnContentID, p.ContentID)
@@ -63,18 +96,12 @@ func (p *Part) Encode(writer io.Writer) error {
 	if p.ContentType != "" {
 		// Build content type header.
 		param := make(map[string]string)
-		if p.Charset != "" {
-			param[hpCharset] = p.Charset
-		}
-		if p.FileName != "" {
-			param[hpName] = quotedString(p.FileName)
-		}
-		if p.Boundary != "" {
-			param[hpBoundary] = p.Boundary
-		}
+		setParamValue(param, hpCharset, p.Charset)
+		setParamValue(param, hpName, quotedString(p.FileName))
+		setParamValue(param, hpBoundary, p.Boundary)
 		mt := mime.FormatMediaType(p.ContentType, param)
 		if mt == "" {
-			// There was an error, FormatMediaType couldn't encode it with the params.
+			// There was an error, FormatMediaType couldn't encode the params.
 			mt = p.ContentType
 		}
 		p.Header.Set(hnContentType, mt)
@@ -82,57 +109,19 @@ func (p *Part) Encode(writer io.Writer) error {
 	if p.Disposition != "" {
 		// Build disposition header.
 		param := make(map[string]string)
-		if p.FileName != "" {
-			param[hpFilename] = quotedString(p.FileName)
+		setParamValue(param, hpFilename, quotedString(p.FileName))
+		mt := mime.FormatMediaType(p.Disposition, param)
+		if mt == "" {
+			// There was an error, FormatMediaType couldn't encode the params.
+			mt = p.Disposition
 		}
-		p.Header.Set(hnContentDisposition, mime.FormatMediaType(p.Disposition, param))
+		p.Header.Set(hnContentDisposition, mt)
 	}
-	// Encode this part.
-	b := bufio.NewWriter(writer)
-	if err := p.encodeHeader(b); err != nil {
-		return err
-	}
-	if len(p.Content) > 0 {
-		if _, err := b.Write(crnl); err != nil {
-			return err
-		}
-		if err := p.encodeContent(b, cte); err != nil {
-			return err
-		}
-		if _, err := b.Write(crnl); err != nil {
-			return err
-		}
-	}
-	if p.FirstChild == nil {
-		return b.Flush()
-	}
-	// Encode children.
-	endMarker := []byte("\r\n--" + p.Boundary + "--")
-	marker := endMarker[:len(endMarker)-2]
-	c := p.FirstChild
-	for c != nil {
-		if _, err := b.Write(marker); err != nil {
-			return err
-		}
-		if _, err := b.Write(crnl); err != nil {
-			return err
-		}
-		if err := c.Encode(b); err != nil {
-			return err
-		}
-		c = c.NextSibling
-	}
-	if _, err := b.Write(endMarker); err != nil {
-		return err
-	}
-	if _, err := b.Write(crnl); err != nil {
-		return err
-	}
-	return b.Flush()
+	return cte
 }
 
 // encodeHeader writes out a sorted list of headers.
-func (p *Part) encodeHeader(b *bufio.Writer) error {
+func (p *Part) encodeHeader(b *bufio.Writer) {
 	keys := make([]string, 0, len(p.Header))
 	for k := range p.Header {
 		keys = append(keys, k)
@@ -150,12 +139,9 @@ func (p *Part) encodeHeader(b *bufio.Writer) error {
 			// _ used to prevent early wrapping
 			wb := stringutil.Wrap(76, k, ":_", encv, "\r\n")
 			wb[len(k)+1] = ' '
-			if _, err := b.Write(wb); err != nil {
-				return err
-			}
+			b.Write(wb)
 		}
 	}
-	return nil
 }
 
 // encodeContent writes out the content in the selected encoding.
@@ -165,21 +151,18 @@ func (p *Part) encodeContent(b *bufio.Writer, cte transferEncoding) (err error) 
 		enc := base64.StdEncoding
 		text := make([]byte, enc.EncodedLen(len(p.Content)))
 		base64.StdEncoding.Encode(text, p.Content)
+		// Wrap lines.
 		lineLen := 76
 		for len(text) > 0 {
-			// Wrap lines
 			if lineLen > len(text) {
 				lineLen = len(text)
 			}
 			if _, err = b.Write(text[:lineLen]); err != nil {
 				return err
 			}
-			if _, err = b.Write(crnl); err != nil {
-				return err
-			}
+			b.Write(crnl)
 			text = text[lineLen:]
 		}
-		_, err = b.Write(text)
 	case teQuoted:
 		qp := quotedprintable.NewWriter(b)
 		if _, err = qp.Write(p.Content); err != nil {
@@ -201,7 +184,7 @@ func selectTransferEncoding(content []byte, quoteLineBreaks bool) transferEncodi
 	threshold := b64Percent * 100 / len(content)
 	bincount := 0
 	for _, b := range content {
-		if (b < ' ' || b > '~') && b != '\t' {
+		if (b < ' ' || '~' < b) && b != '\t' {
 			if !quoteLineBreaks && (b == '\r' || b == '\n') {
 				continue
 			}
@@ -223,4 +206,11 @@ func quotedString(s string) string {
 		return strings.Trim(strconv.QuoteToASCII(s), `"`)
 	}
 	return s
+}
+
+// setParamValue will ignore empty values
+func setParamValue(p map[string]string, k, v string) {
+	if v != "" {
+		p[k] = v
+	}
 }
