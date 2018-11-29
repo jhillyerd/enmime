8d0f5ec5561e0c30c01e32d8392ceb42fe7dfc81
Kevin <>
2018-11-29 14:22:07 -0800

    Remove CTE header if set in a part before encoding it.

--- encode.go
+++ encode.go
@@ -67,6 +67,11 @@ func (p *Part) Encode(writer io.Writer) error {
 // then sets the Content-Type (type, charset, filename, boundary) and Content-Disposition headers.
 func (p *Part) setupMIMEHeaders() transferEncoding {
 	// Determine content transfer encoding.
+
+	// If we are encoding a part that previously had content-transfer-encoding set, unset it so
+	// the correct encoding detection can be done below.
+	p.Header.Del(hnContentEncoding)
+
 	cte := te7Bit
 	if len(p.Content) > 0 {
 		cte = teBase64
