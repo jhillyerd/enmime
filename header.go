76255ebccafd39b1e2ecee110d3288ec44610d05
Neil <neils@knowbe4.com>
2019-02-06 14:46:39 -0500

    return empty string and nil error on specific mime: no media type error

--- header.go
+++ header.go
@@ -224,11 +224,11 @@ func quotedDisplayName(s string) string {
 func ParseMediaType(ctype string) (mtype string, params map[string]string, invalidParams []string, err error) {
 	mtype, params, err = mime.ParseMediaType(ctype)
 	if err != nil {
-		// Small hack to remove harmless charset duplicate params.
-		mctype := fixMangledMediaType(ctype, ";")
-		if mctype == "" {
+		if err.Error() == "mime: no media type" {
 			return "", nil, nil, nil
 		}
+		// Small hack to remove harmless charset duplicate params.
+		mctype := fixMangledMediaType(ctype, ";")
 		mtype, params, err = mime.ParseMediaType(mctype)
 		if err != nil {
 			// Some badly formed media types forget to send ; between fields.
