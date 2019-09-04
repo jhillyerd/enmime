f9d032263dbe6be4dc6ff8619968c18e5bf9ff62
Neil <neils@knowbe4.com>
2019-09-04 14:54:06 -0400

    use bytes.Contains versus bytes.Index with comparison

--- inspect.go
+++ inspect.go
@@ -57,7 +57,7 @@ func ensureHeaderBoundary(b []byte) []byte {
 	dest := make([]byte, 0, len(b)+2)
 	headers := true
 	for _, v := range slice {
-		if headers && (bytes.Index(v, []byte{':'}) > -1 || bytes.HasPrefix(v, []byte{' '}) || bytes.HasPrefix(v, []byte{'\t'})) {
+		if headers && (bytes.Contains(v, []byte{':'}) || bytes.HasPrefix(v, []byte{' '}) || bytes.HasPrefix(v, []byte{'\t'})) {
 			dest = append(dest, v...)
 			continue
 		}
