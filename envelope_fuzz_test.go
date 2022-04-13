020a63cd91311eaaff14e774e0eed03e1433b8e2
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2022-04-13 12:31:53 +0400

    Add go 1.18

---
+++ envelope_fuzz_test.go
@@ -0,0 +1,27 @@
+//go:build go1.18
+// +build go1.18
+
+package enmime
+
+import (
+	"bytes"
+	"testing"
+)
+
+func FuzzReadEnvelope(f *testing.F) {
+	f.Add([]byte(`Date: Mon, 23 Jun 2015 11:40:36 -0400
+From: Gopher <from@example.com>
+To: Example User <user@example.com>
+Subject: Gophers at Gophercon
+
+Message body
+
+
+`))
+	f.Fuzz(func(t *testing.T, b []byte) {
+		envelope, err := ReadEnvelope(bytes.NewReader(b))
+		if envelope != nil && err != nil {
+			t.Error("envelope and error are not nil")
+		}
+	})
+}
