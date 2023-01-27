54324c8c2c1cd5597ca5b86fbd45be4053d3b477
James Hillyerd <james@hillyerd.com>
2023-01-27 09:51:53 -0800

    Use Go 1.20 from nixpkgs-unstable for dev shell
    Signed-off-by: James Hillyerd <james@hillyerd.com>

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShell = pkgs.callPackage ./shell.nix { };
+        }
+      );
+}
